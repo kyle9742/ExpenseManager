a96c08179d531c35c15917085964188f5b9d266a
KYLE <kyle9742@naver.com>
2024-05-21 09:29:46 +0900

    시큐리티 인증 로그인 정적파일 설정

--- src/main/java/com/mysite/expense/config/CustomSecurityConfig.java
+++ src/main/java/com/mysite/expense/config/CustomSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,11 +21,25 @@ public class CustomSecurityConfig {
                         authz
                                 .requestMatchers("/js/**", "/css/**").permitAll()
                                 .requestMatchers("/", "/login", "/register").permitAll()
+                                .anyRequest().authenticated()
+                ) // 리퀘스트매처의 주소들은 허가하고 그 외의 주소는 인증을 요구함
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .failureUrl("/")
+                        .defaultSuccessUrl("/expenses")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        // 인증시 로그인 페이지를 지정하고 실패시 주소 지정 성공주소 지정 (username => email, password)
                 )
-
         ;
 
         return http.build();
+    }
+
+    // 정적 파일을 예외로 처리하기 (시큐리티의 예외)
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/css/**", "/js/**", "/error");
     }
 
     @Bean
